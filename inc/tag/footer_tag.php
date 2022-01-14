5a392dc6652d73d02644b75d842176fd48990e0f
bpoc-mark <markariel.maata@gmail.com>
2022-01-14 16:33:19 +0800

    update font

--- inc/tag/footer_tag.php
+++ inc/tag/footer_tag.php
@@ -1,10 +1,10 @@
-<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="/js/jquery.matchHeight-min.js"></script>
 <script src="/js/polyfill.js"></script>
 <script src="/js/smooth-scroll.polyfills.min.js"></script>
-<script src="/js/lazysizes.min.js"></script> -->
+<script src="/js/lazysizes.min.js"></script>
 <script src="/js/common.js"></script>
-<!-- <script>
+<script>
   $(window).on("load", function() {
     let scroll = new SmoothScroll('a[href*="#"]', {
       header: "header",
@@ -15,4 +15,4 @@
   });
 
   $('.fot_top nav li').matchHeight();
-</script> -->
+</script>
